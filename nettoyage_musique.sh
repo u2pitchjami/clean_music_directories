a328eef0e42ea161dbb56409713ca23977a07cfd
TB <149841209+u2pitchjami@users.noreply.github.com>
2024-07-03 12:15:27 +0200

    cleanup

--- nettoyage_musique.sh
+++ nettoyage_musique.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
+######CONFIGURATION###########################################################
 DATE=$(date +%y%m%d)
-DOSSIER_MUSIC=/mnt/user/Medias/"Shared Music"/Collection/
-DOSSIER_TEMP=/mnt/user/Medias/${DATE}_temp_clean_music
-FICHIER_TEMP=/mnt/user/Medias/${DATE}_temp_clean_music.txt
-CORBEILLE=/mnt/user/Medias/Corbeille/
-LOG=/mnt/user/Documents/scripts/logs/${DATE}clean_music_log.txt
+DOSSIER_MUSIC=/path/to/music/base/
+DOSSIER_TEMP=/temp/folder/path/${DATE}_temp_clean_music #will be deleted at the end
+FICHIER_TEMP=/temp/file/path/${DATE}_temp_clean_music.txt #will be deleted at the end
+CORBEILLE=/path/to/trash/folder/
+LOG=/path/to/logs/files/${DATE}clean_music_log.txt
+######CONFIGURATION###########################################################
 
 logit() 
 {
@@ -29,22 +32,19 @@ sed 's/^ *//' < "${FICHIER_TEMP}" | xargs -d '\n' mv --backup=numbered -t "${DOS
 
 logit "Vérification des dossiers transférés :"
 NB=$(find "${DOSSIER_TEMP}" -type f \( -iname "*.mp3" -o -iname "*.mp4" -o -iname "*.flac" -o -iname "*.wav" -o -iname "*.ogg" -o -iname "*.aac" -o -iname "*.wma" -o -iname "*.aiff" -o -iname "*.m4a" \) -print | wc -l)
-if [ $NB=0 ]; then
-logit "Aucun fichier audio, c'est top mec"
-mv ${DOSSIER_TEMP} ${CORBEILLE}
-mv ${FICHIER_TEMP} ${CORBEILLE}
 
-logit "Dossiers placés dans la corbeille pour 7 jours"
+    if [ $NB=0 ]; then
+    logit "Aucun fichier audio, c'est top mec"
+    mv ${DOSSIER_TEMP} ${CORBEILLE}
+    mv ${FICHIER_TEMP} ${CORBEILLE}
+    logit "Dossiers placés dans la corbeille pour 7 jours"
+    echo "cool, c'est fini"
+    logit "cool, c'est fini !!!!"
+    else
+    logit "pas cool, il reste des fichiers audios"
+    echo "pas cool, il reste des fichiers audios"
+    fi
 
-echo "cool, c'est fini"
-
-logit "cool, c'est fini !!!!"
-
-else
-logit "pas cool, il reste des fichiers audios"
-echo "pas cool, il reste des fichiers audios"
-
-fi
 logit "suppression des dossiers vides"
 find "${DOSSIER_MUSIC}" -empty -type d -print -delete | tee -a "${LOG}"
 echo
